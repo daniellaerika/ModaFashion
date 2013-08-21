44ee96ceebe0150b3ac2c137c1aadda69645e608
20112144110309 <20112144110309@PAR155978.ifrn.local>
2013-08-21 19:23:24 -0300

    adicionando o projeto

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <center>
+    <meta charset="utf-8">
+ 
+		<title>Acessórios Femininos</title>
+		<link rel="stylesheet" type="text/css" href="Estilos/estilo01.css">
+	</head>
+    <body>
+        
+    </body>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+	<link href="estilos/style.css" rel="stylesheet">
+    <link href="../bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      /* Override some defaults */
+      html, body {
+        background-color: #eee;
+      }
+      body {
+        padding-top: 40px; 
+      }
+      
+
+      /* The white background content wrapper */
+      .container > .content {
+        background-color: bisque;
+        padding: 20px;
+        margin: 0 -20px; 
+        -webkit-border-radius: 10px 10px 10px 10px;
+           -moz-border-radius: 10px 10px 10px 10px;
+                border-radius: 10px 10px 10px 10px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.15);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.15);
+                box-shadow: 0 1px 2px rgba(0,0,0,.15);
+      }
+
+	  .login-form {
+		margin-left: 20px;
+	  }
+	
+	  legend {
+		margin-right: -50px;
+		font-weight: bold;
+	  	color: #404040;
+	  }
+	  
+
+
+
+
+    </style>
+
+</head>
+
+
+<body>
+
+ <div id="topo">
+  <p><a href="http://www.google.com"><img src="Imagens/01.jpg" alt="Google" width="300" height="300"/></a></p>
+ </div>
+
+ <div id="corpo">
+     
+ 
+ </div>
+ 
+ 
+ 
+  <h2><center>Este é um site de coisas femininas, onde você pode explorar o máximo das tendências do momento</li></center>
+	<center>seja roupas, sapatos, bolsas ou acessórios. Enfim divirtam-se com as dicas e boa sorte na escolha<p></h2></li></center>
+	<br>
+    <center><li><a href="#cap01">Roupas</a></li>
+    <li><a href="#cap02">Sapatos</a></li>
+	<li><a href="#cap03">Acessórios de cabelo</a></li></center>
+</body>
+</html>
+</center>
+</div>
+</br>
+
+
+
+<div id="rodape">
+
+<center><a name="cap01"/>
+	<h2> Roupas</h2>
+	<p><ul type ="circle">
+	<li> Vestidos são a ultima moda neste inverno, como também o uso de roupas pesadas e estampas em xadrex retrô.</li>
+	<li>Saias de cintura alta, estão dando toque clássico no guarda-roupa das mulheres brasileiras.</li>
+	<li>Sapatilhas são os sapatos que substituíram o salto alto, com vários modelos e formas elegantes que pode se usar casualmente</li>
+	</ul></p>
+	<p><a href="http://www.google.com"><img src="Imagens/02.jpg" alt="Google" width="500" height="328"/></a></p></center>
+	
+	<center><a name="cap02"/>
+	<h2> Sapatos</h2>
+	<p>São a sensação do momento entre as mulheres, pois ele é o que faz de uma mulher se sentir bela e cheia de estilo.</li>
+	<p><a href="http://www.google.com"><img src="Imagens/03.jpg" alt="Google" width="500" height="328"/></a></p>
+	</ul></p></center>
+	<br>
+	<br>
+	
+	<center><a name="cap03"/>
+	<h2> Acessórios de cabelo</h2>
+	<p>Iremos ver os estilos de acessórios que fazem o penteado da mulher mais lindo do mundo, com simplicidade e beleza.</li>
+	<p><a href="http://www.google.com"><img src="Imagens/04.jpg" alt="Google" width="500" height="328"/></a></p></center>
+	</ul></p>
+	
+	
+</div>
+</center>
+
+</body>
+
+
+</html>
